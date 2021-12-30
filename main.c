277dcfa31a37bda063835f8eea44b307aced4465
IvanSmirnovSPL <smirnov.ie@ohystech.edu>
2021-12-30 11:46:29 +0300

    bar

--- main.c
+++ main.c
@@ -10,29 +10,27 @@
 
 int main() {
 
-    int flag, sum = 0;/* calculate number of errors*/
-    bool Q = 1;/* show details if '1', doesn't show if '0'*/
-    flag = test_cross_product(Q); sum += flag; if (flag) flag = test_cross_product(1);
-    flag = test_scaling(Q); sum += flag; if (flag) flag = test_scaling(1);
-    flag = test_m_from_b_only(Q); sum += flag; if (flag) flag = test_m_from_b_only(1);
-    flag = test_m_from_b_and_w(Q); sum += flag; if (flag) flag = test_m_from_b_and_w(1);
-    flag = test_check_w_working(Q); sum += flag; if (flag) flag = test_check_w_working(1);
-    flag = test_make_m_percent(Q); sum += flag; if (flag) flag = test_make_m_percent(1);
-    flag = test_deg_to_rad(Q); sum += flag; if (flag) flag = test_deg_to_rad(1);
-    flag = test_derivative(Q); sum += flag; if (flag) flag = test_derivative(1);
-    flag = test_average(Q); sum += flag; if (flag) flag = test_average(1);
-    flag = test_weighted_moving_average(Q); sum += flag; if (flag) flag = test_weighted_moving_average(1);
-    flag = test_calculate_magnetic_moment(Q); sum += flag; if (flag) flag = test_calculate_magnetic_moment(1);
+    int sum = 0;/* calculate number of errors*/
+    printf("\n");
+    sum += test_cross_product();
+    sum += test_scaling();
+    sum += test_m_from_b_only();
+    sum += test_m_from_b_and_w();
+    sum += test_check_w_working();
+    sum += test_make_m_percent();
+    sum += test_deg_to_rad();
+    sum += test_derivative();
+    sum += test_average();
+    sum += test_weighted_moving_average();
+    sum += test_calculate_magnetic_moment();
 
     if (sum == 0)
     {
-        printf("\n \n==============================================="
-               " ALL RIGHT ==============================================\n");
+        printf("\nall test completed successfully \n");
     }
     else
     {
-        printf("\n \n=============================================="
-               " %d PROBLEMS ========================================\n", sum);
+        printf("\n%d tests failed \n", sum);
     }
 
     return 0;
--- test_functions.h
+++ test_functions.h
@@ -11,16 +11,16 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-test_calculate_magnetic_moment(bool q);
-test_weighted_moving_average(bool q);
-test_average(bool q);
-test_derivative(bool q);
-test_deg_to_rad(bool q);
-test_make_m_percent(bool q);
-test_check_w_working(bool q);
-test_m_from_b_and_w(bool q);
-test_m_from_b_only(bool q);
-test_scaling(bool q);
-test_cross_product(bool q);
+test_calculate_magnetic_moment();
+test_weighted_moving_average();
+test_average();
+test_derivative();
+test_deg_to_rad();
+test_make_m_percent();
+test_check_w_working();
+test_m_from_b_and_w();
+test_m_from_b_only();
+test_scaling();
+test_cross_product();
 
 #endif //B_DOT_TEST_FUNCTIONS_H
